dc865ed7bca89f19df02c10ed5419cbecddcf2f7
KangJun188247 <1640400668@qq.com>
2022-10-25 16:15:38 +0800

    '本次提交为测试'

---
+++ src/views/DiscovrView/DiscovrView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="discover-view">
+    <div class="search-tab">
+      <van-search
+        class="search-input"
+        shape="round"
+        background="#fff"
+        placeholder="搜索歌曲"
+      />
+      <div class="music-img"></div>
+    </div>
+
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.discover-view {
+  .search-tab {
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: auto;
+    z-index: 10;
+    width: 100vw;
+    background-color: #fff;
+
+    .van-search {
+      width: 85vw;
+    }
+
+    .music-img {
+      width: 35px;
+      height: 35px;
+      background-color: rgb(227, 111, 111);
+      border-radius: 999px;
+    }
+  }
+
+}
+</style>
