0ec640f3c9f01dd01edefa6ac7f9b6563402bd56
KangJun188247 <1640400668@qq.com>
2022-10-27 20:46:59 +0800

    做了新音乐组件

--- src/views/DiscovrView/DiscoverContent.vue
+++ src/views/DiscovrView/DiscoverContent.vue
@@ -2,20 +2,35 @@
   <div class="discover-content">
     <DiscoverBanner :bannerimgdata="bannerimgdata" />
     <HomepageItemList :HomepageItemList="HomepageItemList" />
+    <NewMusic :NewMusic="NewMusic"/>
     <RecommendSong :RecommendSong="RecommendSong" />
   </div>
 </template>
 <script>
 import DiscoverBanner from "@/components/DiscoverBanner.vue";
 import RecommendSong from "@/components/RecommendSong.vue";
+import NewMusic from "@/components/NewMusic.vue";
+
 import HomepageItemList from "@/components/HomepageItemList.vue";
+// nav icon
 import { getHomepageItemList } from "../../apis/discover.js";
+// 推荐歌单
 import { getPlaylistdata } from "../../apis/index.js";
+// 新音乐
+import { NEWSONGSAPI } from "../../apis/play.js";
+// 电台
+import { getRadioStationData } from "../../apis/radiostation.js";
 export default {
   data() {
     return {
+      // nav icon
       HomepageItemList: [],
+      // 推荐歌单
       RecommendSong: [],
+      // 新音乐
+      NewMusic: [],
+      // 电台
+      radioStation: [],
     };
   },
 
@@ -24,21 +39,37 @@ export default {
   },
 
   mounted() {
+    //
     this.getHomepageItemList();
     this.getPlaylistdata();
+    this.getnewsongsapi();
+    this.getradiostation();
   },
 
   methods: {
+    // nav icon
     async getHomepageItemList() {
       let { data } = await this.$axios(getHomepageItemList);
       this.HomepageItemList = data.data;
       // console.log(data.data);
     },
-
+    // 推荐歌单
     async getPlaylistdata() {
       let { data } = await this.$axios(getPlaylistdata);
       this.RecommendSong = data.result;
-      console.log(data.result);
+      // console.log(data.result);
+    },
+    // 新音乐
+    async getnewsongsapi() {
+      let { data } = await this.$axios(NEWSONGSAPI);
+      this.NewMusic = data.result;
+      console.log("1", data.result);
+    },
+    // 电台
+    async getradiostation() {
+      let { data } = await this.$axios(getRadioStationData);
+      this.radioStation = data.categories;
+      console.log(this.radioStation);
     },
   },
 
@@ -46,6 +77,7 @@ export default {
     DiscoverBanner,
     HomepageItemList,
     RecommendSong,
+    NewMusic,
   },
 };
 </script>
