ed1bdd4d86590e688dbb4fe8e680ef96e025d103
xuan chen <1505622809@qq.com>
2022-10-28 20:22:03 +0800

    添加了video页面

--- src/components/HomepageItemList.vue
+++ src/components/HomepageItemList.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="homepage-content">
+  <div class="recommend-content">
     <div v-for="h in HomepageItemList" :key="h.id">
-      <div class="homepage-list">
-        <div class="homepage-icon-title">
-          <img class="homepage-icon" v-lazy="h.iconUrl" alt="" />
-          <h3>{{ h.name }}</h3>
-        </div>
+      <div class="homepage-icon-title">
+        <img class="recommend-icon" v-lazy="h.iconUrl" alt="" />
+        <h3>{{ h.name }}</h3>
       </div>
     </div>
   </div>
@@ -18,28 +16,28 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.homepage-content {
+.recommend-content {
+  padding: 0 20px;
+  width: 100vw;
   height: 10vh;
   display: flex;
+//   justify-content: space-around;
   align-items: center;
   border-bottom: 1px solid rgb(142, 142, 142);
   background-color: rgb(253, 188, 188);
   font-size: 12px;
   overflow-x: auto;
+//   overflow: hidden;
 
-  .homepage-list {
-    width: 100%;
+  .homepage-icon-title {
     display: flex;
     flex-flow: column;
     justify-content: center;
     text-align: center;
-    padding: 0 10px;
-    .homepage-icon-title {
-      width: 100%;
-      .homepage-icon {
-        width: 50x;
-        height: 50px;
-      }
+
+    .recommend-icon {
+      width: 50x;
+      height: 50px;
     }
   }
 }
