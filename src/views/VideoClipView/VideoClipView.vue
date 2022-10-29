06a459c8dcf76646e5ef52ad65cd9c04cf3ebfb3
kinomo2 <2053565729@qq.com>
2022-10-29 17:34:16 +0800

    测试-时间2022年10月29日17:34:13

--- src/views/VideoClipView/VideoClipView.vue
+++ src/views/VideoClipView/VideoClipView.vue
@@ -1,20 +1,24 @@
 <template>
- <div >
+<div >
   
     <!-- <div v-for="(m,i) in mvList" :key="i.artistId"  > -->
          
-          <div v-for="m in mvList" :key="m.id"  >
-            <div @click="playMV(m.id)"> 视频 {{m.id}}</div>
-         <video 
-            :id="m.id"
-          :src="mvId.url"
-         width="375px" height="270px"
-         controls 
+    <div v-for="m in mvList" :key="m.id"  >
+      <div  @click="playMV(m.id)"> 视频 {{m.id}}   </div>
+        <div>
 
+       
+         <video 
+         
+          :id="m.id"
+          :src="mvId.url"
+          width="375px" height="270px"
+          controls 
+         
          >
-        
         </video>
-     </div>
+      </div>
+    </div>
 </div>
    
 </template>
@@ -26,7 +30,7 @@ export default {
     data() {
         return {
             mvList:[],
-           mvId:[]
+           mvId:{}
         }
     },
 //     created() {
@@ -41,13 +45,14 @@ export default {
       let { data } = await this.$axios(getMVList);
       this.mvList = data.data;
       console.log(this.mvList);
+      // this.playMV(this.mvList.id)
     },
 
     // 获取MV播放地址
      async   playMV(id) {
       let { data } =  await this.$axios(playMV(id));
       this.mvId = data.data
-      console.log(  this.mvId);
+      console.log( this.mvId);
     },
 
     },
