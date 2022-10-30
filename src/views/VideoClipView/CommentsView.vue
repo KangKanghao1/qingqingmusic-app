bf33e4d760d29994ac90cd64bc3090a4c31af013
kinomo2 <2053565729@qq.com>
2022-10-30 17:59:52 +0800

    更新 video 页面

---
+++ src/views/VideoClipView/CommentsView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="comments-list">
+        <div class="comments-item">
+            <div>头像</div>
+            <div>
+                <div>昵称</div>
+                <div>
+                    2022年10月30日16:18:17
+                </div>
+                <div>评论文本</div>
+            </div>
+        </div>
+        111111111111111111
+    </div>
+</template>
+<style lang="scss" scoped>
+.app {
+
+    .comments {
+    width: 100px;
+    height: 100px;
+    .comments-item {
+        // width: 100vw;
+        height: 60px;
+        border: 1px solid #ddd;
+        display: flex;
+
+    }
+}
+}
+
+</style>
