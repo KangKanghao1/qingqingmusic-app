6d329ce3b68a9e6e547894a9a31bdbedc40034fc
KangJun188247 <1640400668@qq.com>
2022-10-30 10:29:20 +0800

    '更新一下歌手热的'

--- src/components/BangSinger.vue
+++ src/components/BangSinger.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bang-singer">
-      <div class="mv-content">
+    <div class="mv-content">
       <h3 class="mv-title">TOP歌手</h3>
       <span>更多</span>
     </div>
@@ -8,6 +8,15 @@
       <div v-for="t in toplistartistData" :key="t.id">
         <div class="bang-img-title">
           <img class="bang-img" v-lazy="t.picUrl" />
+
+          <div class="score-data">
+            <i>1111</i>
+            <span class="score">{{
+              t.score > 10000
+                ? (t.score = Math.floor(t.score / 1000) / 10 + "万")
+                : t.score
+            }}</span>
+          </div>
 
           <div class="bang-title-alias">
             <h3 class="bang-title">{{ t.name }}</h3>
@@ -38,7 +47,6 @@ export default {
   width: 100%;
   padding: 0px 30px;
 
-
   .mv-content {
     display: flex;
     justify-content: space-between;
@@ -50,7 +58,6 @@ export default {
 
   .bang {
     padding: 15px;
-    padding: 0 0 50px;
 
     .bang-img-title {
       position: relative;
@@ -59,6 +66,21 @@ export default {
       border-radius: 15px;
       overflow: hidden;
       margin-top: 20px;
+      .score-data {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        background-color: rgba(255, 255, 255, 0.5);
+        padding: 10px;
+        border-radius: 999px;
+        font-size: 20px;
+        transform-origin: top right;
+         transform: scale(0.5);
+         color: #ff3333;
+        .score {
+          margin-left: 5px;
+        }
+      }
 
       .bang-img {
         display: block;
@@ -68,20 +90,21 @@ export default {
 
       .bang-title-alias {
         position: absolute;
-        left: 0;
-        bottom: 0;
+        left: 10px;
+        bottom: 10px;
         .bang-title {
           position: absolute;
           left: 0px;
           bottom: 40px;
+          font-size: 30px;
           font-weight: bold;
-          color: rgb(254, 249, 249);
+          color: rgb(250, 87, 87);
         }
 
         .alias {
-          font-size: 16px;
+          font-size: 20px;
           font-weight: bold;
-          color: rgb(249, 245, 245);
+          color: #FF0000 ;
         }
       }
     }
